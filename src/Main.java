85088cb86d12ad5bd2b0f80ab716f15db92ceabc
Lena Koller <lkoller@allinsight.de>
2023-05-17 12:34:28 +0200

    show all

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Exercise 1: Boxes");
+        boxes();
+        System.out.println();
+
+        System.out.println("Exercise 2: Passbook");
         passbook();
     }
 
